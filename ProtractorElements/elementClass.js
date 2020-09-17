4a0296909c0e0e9544452851fa4d128588a9b3b5
rkkiran <rk2003kiran@gmail.com>
2020-09-17 17:34:29 +0100

    added condition

--- ProtractorElements/elementClass.js
+++ ProtractorElements/elementClass.js
@@ -1,131 +1,163 @@
-"use strict";
-const { element, by, browser } = require ('protractor');
+'use strict';
+const { element, by, browser } = require('protractor');
 
- class ElementsClass {
-    /**
-     * 
-     * @param url - url value or link
-     */
-     async getBrowser(url) {
-       return await browser.get(url);
+class ElementsClass {
+  /**
+   *
+   * @param url - url value or link
+   */
+  async getBrowser(url) {
+    return await browser.get(url);
+  }
+  /**
+   *
+   * @param value          - xapth value
+   * @param actionType     - protractor action type like to click() or getText() and more
+   * @param attributeValue - to find unique value I am using the getAttribute function and passing the attribute value
+   */
+  async xpath(value, actionType, attributeValue) {
+    if (attributeValue === undefined) {
     }
-    /**
-     * 
-     * @param value          - xapth value
-     * @param actionType     - protractor action type like to click() or getText() and more
-     * @param attributeValue - to find unique value I am using the getAttribute function and passing the attribute value
-     */
-     async xpath(value, actionType, attributeValue) {
-         if(attributeValue === undefined){};
-        let returnValue = null;
-        switch (actionType) {
-            case 'click':
-                returnValue = await element(by.xpath(value)).click();                             
-               break;
-            case 'isPresent':
-                returnValue = await element(by.xpath(value)).isPresent();
-                break; 
-            case 'getAttribute':
-                returnValue = await element(by.xpath(value)).getAttribute(attributeValue);
-                break;    
-            default:
-                returnValue = await element(by.xpath(value)).getText();
-                break;
-        }
-        return returnValue;
+    let returnValue = null;
+    switch (actionType) {
+      case 'click':
+        returnValue = await element(by.xpath(value)).click();
+        break;
+      case 'isPresent':
+        returnValue = await element(by.xpath(value)).isPresent();
+        break;
+      case 'getAttribute':
+        returnValue = await element(by.xpath(value)).getAttribute(
+          attributeValue,
+        );
+        break;
+      default:
+        returnValue = await element(by.xpath(value)).getText();
+        break;
     }
-    /**
-     * 
-     * @param value       - css value
-     * @param actionType  - protractor action type like to click() or getText() and more
-     * @param sendKeys    - to send the values in the text box element for example to login 
-     */
-     async css(value, actionType, sendKeys, attributeValue) { 
-        if(sendKeys && attributeValue === undefined){}
-        let returnValue = null;
-        switch (actionType) {
-            case 'click':
-               returnValue = await element(by.css(value)).click();                             
-               break;
-            case 'sendKeys':
-                returnValue = await element(by.css(value)).sendKeys(sendKeys);
-                break;        
-            default:
-                returnValue = await element(by.css(value)).getText();
-                break;
-        }
-        return returnValue;
+    return returnValue;
+  }
+  /**
+   *
+   * @param value       - css value
+   * @param actionType  - protractor action type like to click() or getText() and more
+   * @param sendKeys    - to send the values in the text box element for example to login
+   */
+  async css(value, actionType, sendKeys, attributeValue) {
+    if (sendKeys && attributeValue === undefined) {
     }
-    /**
-     * 
-     * @param value          - id value 
-     * @param actionType     - protractor action type like to click() or getText() and more
-     * @param sendKeysValue  - to send the values in the text box element for example to login  
-     */
-     async id(value, actionType, sendKeysValue) { 
-        if(sendKeysValue === undefined){}
-        let returnValue = null;
-        switch (actionType) {
-            case 'click':
-               returnValue = await element(by.id(value)).click();                             
-               break;
-            case 'sendKeys':
-                returnValue = await element(by.id(value)).sendKeys(sendKeysValue);
-                break;
-            case 'isPresent':
-                    returnValue = await element(by.xpath(value)).isPresent();
-                    break;        
-            default:
-                returnValue = await element(by.id(value)).getText();
-                break;
-        }
-        return returnValue;
+    let returnValue = null;
+    switch (actionType) {
+      case 'click':
+        returnValue = await element(by.css(value)).click();
+        break;
+      case 'sendKeys':
+        returnValue = await element(by.css(value)).sendKeys(sendKeys);
+        break;
+      default:
+        returnValue = await element(by.css(value)).getText();
+        break;
     }
-    async mouseHover(selector, parentElement, childSelector){
-        switch (selector) {
-            case 'id':
-                const mainSelector_id = await element(by.id(parentElement));
-                const subSelector_id = await element(by.id(childSelector));
-               await  browser.actions().mouseMove(mainSelector_id).mouseMove(subSelector_id).click().perform();              
-                break;
-            case 'xpath':
-                const mainSelector_xpath = await element(by.xpath(parentElement));
-                const subSelector_xpath = await element(by.xpath(childSelector));
-               await browser.actions().mouseMove(mainSelector_xpath).mouseMove(subSelector_xpath).click().perform();                
-                break;
-        
-            default:
-                const mainSelector = await element(by.css(parentElement));
-                const subSelector = await element(by.css(childSelector));
-               await browser.actions().mouseMove(mainSelector).mouseMove(subSelector).click().perform(); 
-                break;
-        }
+    return returnValue;
+  }
+  /**
+   *
+   * @param value          - id value
+   * @param actionType     - protractor action type like to click() or getText() and more
+   * @param sendKeysValue  - to send the values in the text box element for example to login
+   */
+  async id(value, actionType, sendKeysValue) {
+    if (sendKeysValue === undefined) {
     }
-    /**
-     * 
-     * @param value - css value
-     * @param text  - css value text
-     */
-     async cssContainingText(value, text) { 
-        return await element(by.cssContainingText(value, text)).click();
+    let returnValue = null;
+    switch (actionType) {
+      case 'click':
+        returnValue = await element(by.id(value)).click();
+        break;
+      case 'sendKeys':
+        returnValue = await element(by.id(value)).sendKeys(
+          sendKeysValue,
+        );
+        break;
+      case 'isPresent':
+        returnValue = await element(by.xpath(value)).isPresent();
+        break;
+      default:
+        returnValue = await element(by.id(value)).getText();
+        break;
     }
-    /**
-     * 
-     * @param value - xpath value
-     */
-     async elementAllXpath(value){
-        return await element.all(by.xpath(value)).click();
+    return returnValue;
+  }
+  async mouseHover(selector, parentElement, childSelector) {
+    switch (selector) {
+      case 'id':
+        const mainSelector_id = await element(by.id(parentElement));
+        const subSelector_id = await element(by.id(childSelector));
+        await browser
+          .actions()
+          .mouseMove(mainSelector_id)
+          .mouseMove(subSelector_id)
+          .click()
+          .perform();
+        break;
+      case 'xpath':
+        const mainSelector_xpath = await element(
+          by.xpath(parentElement),
+        );
+        const subSelector_xpath = await element(
+          by.xpath(childSelector),
+        );
+        await browser
+          .actions()
+          .mouseMove(mainSelector_xpath)
+          .mouseMove(subSelector_xpath)
+          .click()
+          .perform();
+        break;
+
+      default:
+        const mainSelector = await element(by.css(parentElement));
+        const subSelector = await element(by.css(childSelector));
+        await browser
+          .actions()
+          .mouseMove(mainSelector)
+          .mouseMove(subSelector)
+          .click()
+          .perform();
+        break;
     }
-    /**
-     * 
-     * @param value          - xpath value
-     * @param attributeValue - to find unique value I am using the getAttribute function and passing the attribute value
-     * @param textContains   - using textContains value will find the value in the elment
-     */
-     async returnStringValue(value, attributeValue, textContains){
-        const getElmentValue = await element.all(by.xpath(value)).getAttribute(attributeValue);     
-        return getElmentValue.split(" ").filter(string => string.includes(textContains));
-    }
-    
+  }
+  /**
+   *
+   * @param value - css value
+   * @param text  - css value text
+   */
+  async cssContainingText(value, text) {
+    return await element(by.cssContainingText(value, text)).click();
+  }
+  /**
+   *
+   * @param value - xpath value
+   */
+  async elementAllXpath(value) {
+    return await element.all(by.xpath(value)).click();
+  }
+  /**
+   *
+   * @param value          - xpath value
+   * @param attributeValue - to find unique value I am using the getAttribute function and passing the attribute value
+   * @param textContains   - using textContains value will find the value in the elment
+   */
+  async returnStringValue(value, attributeValue, textContains) {
+    const getElmentValue = await element
+      .all(by.xpath(value))
+      .getAttribute(attributeValue);
+    return getElmentValue
+      .split(' ')
+      .filter((string) => string.includes(textContains));
+  }
+  async scrollUp() {
+    await browser.executeScript('window.scrollTo(0,0);');
+  }
 }
 module.exports = new ElementsClass();
--- pages/productPage/productPage.js
+++ pages/productPage/productPage.js
@@ -1,24 +1,46 @@
-"use strict";
+'use strict';
 var itgWait = require('../../helpers/helper');
-var itgElement  = require('../../ProtractorElements/elementClass');
+var itgElement = require('../../ProtractorElements/elementClass');
 
- class ProductPage {
-   /**
-    * @getProductPrice - will retn the product price
-    */
-    async getProductPrice() {
-      itgWait.waitForElementToBePresent('.//*[@id="priceblock_saleprice"]');
-        const productPriceIs = await itgElement.id('priceblock_saleprice', 'getText');
-        return productPriceIs.substring(1);
-      }
-      /**
-       * @addProductToBasket - will add item to the basket
-       */
-    async addProductToBasket() {
-        itgWait.waitForElementToBeClickable('//input[@id="add-to-cart-button"]');
-        await itgElement.xpath('//input[@id="add-to-cart-button"]', 'click');
-        await itgWait.waitForElementToBePresent('(.//*[@class="a-color-price hlb-price a-inline-block a-text-bold"])[1]');        
+class ProductPage {
+  /**
+   * @getProductPrice - will retn the product price
+   */
+  async getProductPrice() {
+    itgWait.waitForElementToBePresent(
+      './/*[@id="priceblock_saleprice"]',
+    );
+    const productPriceIs = await itgElement.id(
+      'priceblock_saleprice',
+      'getText',
+    );
+    return productPriceIs.substring(1);
+  }
+  /**
+   * @addProductToBasket - will add item to the basket
+   */
+  async addProductToBasket() {
+    const isDealElementPresent = await itgElement.xpath(
+      '//span[@class="a-size-base gb-accordion-inactive"]',
+      'isPresent',
+    );
+    await itgElement.scrollUp();
+    if (isDealElementPresent === true) {
+      await itgElement.xpath(
+        '//span[@class="a-size-base gb-accordion-inactive"]',
+        'click',
+      );
     }
-
+    await itgWait.waitForElementToBeClickable(
+      '//input[@id="add-to-cart-button"]',
+    );
+    await itgElement.xpath(
+      '//input[@id="add-to-cart-button"]',
+      'click',
+    );
+    await itgWait.waitForElementToBePresent(
+      '(.//*[@class="a-color-price hlb-price a-inline-block a-text-bold"])[1]',
+    );
+  }
 }
 module.exports = new ProductPage();
